c7b1d72e84b3fcb8c3af74797c228e9f68ce3e3e
Kyeongweon PARK <semipia@Kyeongweonui-MacBook-Air.local>
2020-07-02 06:09:29 +0900

    Updates

---
+++ precache-manifest.13afc0db545c89510a97491d32752767.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2548f9553325b5a7b5fe2754916f57b7",
+    "url": "/movie_view_2020/index.html"
+  },
+  {
+    "revision": "f78a5348587965b30987",
+    "url": "/movie_view_2020/static/css/main.81b2df77.chunk.css"
+  },
+  {
+    "revision": "865f08adc412f7aa4541",
+    "url": "/movie_view_2020/static/js/2.3ef92743.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_view_2020/static/js/2.3ef92743.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f78a5348587965b30987",
+    "url": "/movie_view_2020/static/js/main.0421f43b.chunk.js"
+  },
+  {
+    "revision": "6643926fdc3b6c8031c8",
+    "url": "/movie_view_2020/static/js/runtime-main.20800191.js"
+  }
+]);
